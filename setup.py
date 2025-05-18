f036a55d4fb1569664fbb567dc968db2805f5596
Sudhanva Bharadwaj <suddusb@gmail.com>
2025-05-18 22:29:30 +0530

    Added requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-MLOps-Project"
+AUTHOR_USER_NAME = "sudhanvabharadwaj"
+SRC_REPO = "mlopsProject"
+AUTHOR_EMAIL = "suddusb@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for an MLOps project",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
