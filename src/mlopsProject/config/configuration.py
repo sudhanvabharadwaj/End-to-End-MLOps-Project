26cfea91c136c3585378750d0d214ccb39e43e6b
Sudhanva Bharadwaj <suddusb@gmail.com>
2025-05-20 18:34:46 +0530

    Added Data Validation

--- src/mlopsProject/config/configuration.py
+++ src/mlopsProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlopsProject.constants import *
 from mlopsProject.utils.common import read_yaml, create_directories
-from mlopsProject.entity.config_entity import DataIngestionConfig
+from mlopsProject.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -25,3 +25,15 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        create_directories([config.root_dir])
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema = schema
+        )
+        return data_validation_config
